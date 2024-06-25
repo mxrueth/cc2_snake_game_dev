08891b67ec9acabf0933a1fa4ac32e9b9311b9a1
warek <marekplichta@gmail.com>
2024-06-25 12:13:57 +0200

    flash effect starting to take shape

--- code/src/vfx.cpp
+++ code/src/vfx.cpp
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+ofVec2f lastEndVec;
+bool executedOnce;
+bool finished;
+int stepper = 200;
 
 vfx::vfx()
 {
@@ -20,7 +24,9 @@ void vfx::generatePoints()
     
         for (int i = 0; i < ofGetWidth(); i= i+25) {
             for (int k = 0; k < ofGetHeight(); k= k+25) {
-                ofVec2f vec(i,k);
+                float ran1 = ofRandom(100);
+                float ran2 = ofRandom(100);
+                ofVec2f vec(i+ran1,k+ran2);
                 allVectors.push_back(vec); //TODO: den vector in der ofApp kreiieren und als referenz passen? damt der von allen flash geteilt wird
             }
         }
@@ -55,31 +61,51 @@ void vfx::spawn(){
     }
     
     //allVectors
-    if(allVectors.size()>0)
-    {
-        //GET THE FIRST TWO POINTS CLOSE TO PLAYER POS
-        //player position
-        ofVec2f originVec(200,500); //TODO: replace with player coordinates
-        //sortiere das array abhaneging vom origin point, thanks AI
-        std::sort(allVectors.begin(), allVectors.end(), [&originVec](const ofVec2f& a, const ofVec2f& b) {
-            return a.distance(originVec) < b.distance(originVec);
-        });
-        //get the first closest point to player
-        ofVec2f closestVec(allVectors[0].x, allVectors[0].y);
-        originPoints.push_back(closestVec); //save for deletion for later
+    if(!finished) {
+        if(allVectors.size()>0)
+        {
+            //GET THE FIRST TWO POINTS CLOSE TO PLAYER POS
+            //player position
+            ofVec2f playerPos(200,500); //TODO: replace with player coordinates
+            ofVec2f startVec;
+            if(!executedOnce){
+                //sortiere das array abhaneging vom origin point, thanks AI
+                std::sort(allVectors.begin(), allVectors.end(), [&playerPos](const ofVec2f& a, const ofVec2f& b) {
+                    return a.distance(playerPos) < b.distance(playerPos);
+                });
+                //get the first closest point to player
+                startVec.x = allVectors[0].x;
+                startVec.y = allVectors[0].y;
+            }else{
+                startVec = lastEndVec;
+                stepper -= 5;
+                if(stepper <= 0) {
+                    finished = true;
+                }
                 
-        //sort array again, starting from that new point
-        std::sort(allVectors.begin(), allVectors.end(), [&closestVec](const ofVec2f& a, const ofVec2f& b) {
-            return a.distance(closestVec) < b.distance(closestVec);
-        });
-        ofVec2f nextClosestVec(allVectors[1].x, allVectors[1].y);
-        originPoints.push_back(nextClosestVec); //save for deletion for later
-                    
-        //verbinden den ersten und Xten punkt mit einer linie
-        ofSetColor(250, 50, 50);
-        ofDrawLine(nextClosestVec.x, nextClosestVec.y, closestVec.x, closestVec.y);
-        //ofDrawLine(100, 100, closestVec.x, closestVec.y);
-  
+            }
+            //originPoints.push_back(closestVec); //save for deletion for later
+            allVectors.erase(allVectors.begin()); //remove point from array
+            
+            //sort array again, starting from that new point
+            std::sort(allVectors.begin(), allVectors.end(), [&startVec](const ofVec2f& a, const ofVec2f& b) {
+                return a.distance(startVec) < b.distance(startVec);
+            });
+            //now we get the closest point to the point that was closest to the player.
+            //if we have been here before then this point will be the closest point to the last end point
+            ofVec2f endVec(allVectors[stepper].x, allVectors[stepper].y);
+            //originPoints.push_back(nextClosestVec); //save for deletion for later
+            //allVectors.erase(allVectors.begin()); //remove point from array as well
+            lastEndVec = endVec;
+            executedOnce = true;
+            
+            //verbinden den ersten und Xten punkt mit einer linie
+            ofSetColor(250, 50, 50);
+            ofSetLineWidth(8);
+            ofDrawLine(endVec.x, endVec.y, startVec.x, startVec.y);
+            //ofDrawLine(100, 100, closestVec.x, closestVec.y);
+            
+        }
     }
         
 }
